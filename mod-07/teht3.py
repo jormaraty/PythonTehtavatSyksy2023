c748ca7dcf601e1ac3c25a311fa56acb25301b1d
jormaraty <jorma.raty@metropolia.fi>
2023-09-14 22:05:25 +0300

    Eka versio.

---
+++ mod-07/teht3.py
@@ -0,0 +1,26 @@
+# Tämä on laaja tehtävä, hyvää harjoittelua projektia varten.
+# Suosittelen vahvasti, että teet eri toiminnot omiin funktioihinsa.
+
+# funktio lisää uuden lentokentän tiedot sanakirjaan.
+# Vaatii lähtötietona sanakirjan, johon lisäys tehdään.
+def lisaa(kentat):
+    # TODO: koodaa sinä, kun ope ei osannut.
+    # kysy käyttäjältä lentokentän ICAO-koodin ja lentokentän nimen.
+
+    # lisää uusi kenttä sanakirjaan.
+    print("Köh, toiminto on vielä työn alla...")
+
+# funktio hakee lentokentän nimen ICAO-koodin avulla.
+# Vaatii lähtötietona lentokentän ICAO-koodin
+def hae(airpots):
+    # TODO: ope ei osannut...
+    print("Ope vielä harjoittelee python-koodausta ja sanakirjan käyttöä.")
+
+
+# -- pääohjelma alkaa --
+# luo uusi sanakirja, johon voit lisätä esim. Helsinki-Vantaan lentoaseman tiedot.
+# Laita tässä luotu sanakirja funktioiden kutsujen parametriksi.
+
+# TODO: Anna käyttäjälle ohjeet ja näytä valikko eri toiminnoista
+# TODO: johonkin pitäis varmaan laittaa toistorakenne, ehkä ...
+
