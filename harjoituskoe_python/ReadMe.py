2ec0be02052dab7429309d4a21e035304fad8f9b
jormaraty <jorma.raty@metropolia.fi>
2023-11-22 07:50:25 +0200

    Tehtävä 1 nyt selkeämpi.

--- harjoituskoe_python/ReadMe.py
+++ harjoituskoe_python/ReadMe.py
@@ -1,5 +1,8 @@
 '''
-    v2.0
+    versio 2a, ke 22.11.2023
+    Muutokset:
+    - tehtävä 1: tehtävää on pyritty selkeyttämään.
+
     Tässä muutamia tehtäviä, joiden tyyppisiä voi tulla kokeeseen.
     Jos saat tulostettua kokeen paperille, niin sitten voi yrittää ratkaista tehtävät kuten kokeessa.
 '''
